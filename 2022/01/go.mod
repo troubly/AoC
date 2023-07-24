265ba4b5ec5744aae705f7f7876bb460a0325b15
Charles Aulagnon <caulagnon@coyote-group.com>
2023-07-24 10:23:00 +0200

    01

---
+++ 2022/01/go.mod
@@ -0,0 +1,5 @@
+module github.com/troubly/AoC/2022/01
+
+go 1.19
+
+require github.com/pkg/errors v0.9.1
---
+++ 2022/01/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	inputFilename = "input"
+	nbTopElves    = 3
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Printf("run failed: %v\n", err)
+	}
+}
+
+func run() error {
+	data, err := os.ReadFile(inputFilename)
+	if err != nil {
+		return errors.Wrap(err, "os.ReadFile")
+	}
+
+	lines := strings.Split(string(data), "\n")
+
+	var (
+		caloriesByElf []int64
+		calories      int64
+	)
+
+	for i, l := range lines {
+		l := strings.TrimSpace(l)
+		if l == "" {
+			caloriesByElf = append(caloriesByElf, calories)
+			calories = 0
+
+			continue
+		}
+
+		c, err := strconv.ParseInt(l, 10, 64)
+		if err != nil {
+			return errors.Wrapf(err, "strconv.ParseInt (line %d)", i)
+		}
+
+		calories += c
+	}
+
+	if calories > 0 {
+		caloriesByElf = append(caloriesByElf, calories)
+	}
+
+	sort.Slice(caloriesByElf, func(i, j int) bool {
+		return caloriesByElf[i] < caloriesByElf[j]
+	})
+
+	nbElves := len(caloriesByElf)
+	if nbElves < nbTopElves {
+		return fmt.Errorf("invalid input: expected at least %d elves, got %d\n", nbTopElves, nbElves)
+	}
+
+	var totalCaloriesTopElves int64
+
+	for i := 0; i < nbTopElves; i++ {
+		totalCaloriesTopElves += caloriesByElf[nbElves-1-i]
+	}
+
+	fmt.Printf("totalCaloriesTopElves: %d\n", totalCaloriesTopElves)
+
+	return nil
+}
